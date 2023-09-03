a54d3b7b52b32e582bb6b2b247bdc62d7b713c93
Rajit Deb <rajitdeb456@gmail.com>
2023-09-03 09:49:33 +0530

    Solved field validation in DetailActivity and added try-catch block to DBHelper for safe accessing of the object

--- app/src/main/java/com/example/justeat/DetailActivity.java
+++ app/src/main/java/com/example/justeat/DetailActivity.java
@@ -3,7 +3,9 @@ package com.example.justeat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteCantOpenDatabaseException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,95 +23,133 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // using database now
-        DBHelper helper = new DBHelper(this);
+        try (DBHelper helper = new DBHelper(this)) {
 
-        if(getIntent().getIntExtra("type", 0) == 1) {
+            if (getIntent().getIntExtra("type", 0) == 1) {
 
-        // Abb yahan intent receive karna hai
-        final int image = getIntent().getIntExtra("image", 0);
-        final int price = Integer.parseInt(getIntent().getStringExtra("price"));
-        final String name = getIntent().getStringExtra("name");
-        final String description = getIntent().getStringExtra("desc");
+                // Abb yahan intent receive karna hai
+                final int image = getIntent().getIntExtra("image", 0);
+                final int price = Integer.parseInt(getIntent().getStringExtra("price"));
+                final String name = getIntent().getStringExtra("name");
+                final String description = getIntent().getStringExtra("desc");
 
-        binding.detailFoodItemImage.setImageResource(image);
-        binding.priceLbl.setText(String.format("%d", price));
-        binding.detailFoodItemName.setText(name);
-        binding.detailDescription.setText(description);
-        binding.detailFoodItemQtyTv.setText(String.valueOf(foodQty));
+                binding.detailFoodItemImage.setImageResource(image);
+                binding.priceLbl.setText(String.format("%d", price));
+                binding.detailFoodItemName.setText(name);
+                binding.detailDescription.setText(description);
+                binding.detailFoodItemQtyTv.setText(String.valueOf(foodQty));
 
-        binding.detailFoodItemQtyAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addFoodQuantity();
+                binding.detailFoodItemQtyAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        addFoodQuantity();
+                    }
+                });
+
+                binding.detailFoodItemQtySub.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        subtractFoodQuantity();
+                    }
+                });
+
+                binding.insertBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        boolean isValidString = checkForValidStringInput();
+
+                        if (isValidString) {
+                            boolean isInserted = helper.insertOrder(
+                                    binding.nameBox.getText().toString(),
+                                    binding.phoneBox.getText().toString(),
+                                    price,
+                                    image,
+                                    name,
+                                    description,
+                                    Integer.parseInt(binding.detailFoodItemQtyTv.getText().toString())
+                            );
+
+                            if (isInserted)
+                                Toast.makeText(DetailActivity.this, "Data Success", Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(DetailActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            } else {
+                // Update Orders
+
+                int id = getIntent().getIntExtra("id", 0);
+                Cursor cursor = helper.getOrderById(id);
+                int image = cursor.getInt(4);
+
+                binding.detailFoodItemImage.setImageResource(image);
+                binding.priceLbl.setText(String.format("%d", cursor.getInt(3)));
+                binding.nameBox.setText(cursor.getString(6));
+                binding.detailDescription.setText(cursor.getString(5));
+
+                binding.nameBox.setText(cursor.getString(1));
+                binding.phoneBox.setText(cursor.getString(2));
+
+                binding.insertBtn.setText("Update Now");
+                binding.insertBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        // checks if the Name and Phone fields are valid inputs
+                        boolean isValidString = checkForValidStringInput();
+
+                        Toast.makeText(DetailActivity.this, "isValidString: " + isValidString, Toast.LENGTH_SHORT).show();
+
+                        if (isValidString) {
+                            boolean isUpdated = helper.updateOrder(
+                                    binding.nameBox.getText().toString(),
+                                    binding.phoneBox.getText().toString(),
+                                    Integer.parseInt(binding.priceLbl.getText().toString()),
+                                    image,
+                                    binding.detailDescription.getText().toString(),
+                                    binding.nameBox.getText().toString(),
+                                    1,
+                                    id
+                            );
+
+                            if (isUpdated)
+                                Toast.makeText(DetailActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(DetailActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
             }
-        });
 
-        binding.detailFoodItemQtySub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                subtractFoodQuantity();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("DB Exception", "DB Exception Occurred: " + e);
+        }
+
+    }
+
+    private boolean checkForValidStringInput() {
+
+        boolean isValid = false;
+
+        String name = binding.nameBox.getText().toString().trim();
+        String phone = binding.phoneBox.getText().toString().trim();
+
+        if (!name.isEmpty()) {
+            if (phone.length() == 10) {
+                isValid = true;
+            } else {
+                Toast.makeText(this, "Please enter a valid Phone Number", Toast.LENGTH_SHORT).show();
             }
-        });
+        } else {
+            Toast.makeText(this, "Please enter your Name", Toast.LENGTH_SHORT).show();
+        }
 
-        binding.insertBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean isInserted = helper.insertOrder(
-                        binding.nameBox.getText().toString(),
-                        binding.phoneBox.getText().toString(),
-                        price,
-                        image,
-                        name,
-                        description,
-                        Integer.parseInt(binding.detailFoodItemQtyTv.getText().toString())
-                );
-
-                if (isInserted)
-                    Toast.makeText(DetailActivity.this, "Data Success", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(DetailActivity.this, "Error", Toast.LENGTH_SHORT).show();
-            }
-        });
-      }
-      else
-      {
-          // Update Orders
-
-          int id = getIntent().getIntExtra("id", 0);
-          Cursor cursor = helper.getOrderById(id);
-          int image = cursor.getInt(4);
-
-          binding.detailFoodItemImage.setImageResource(image);
-          binding.priceLbl.setText(String.format("%d", cursor.getInt(3)));
-          binding.nameBox.setText(cursor.getString(6));
-          binding.detailDescription.setText(cursor.getString(5));
-
-          binding.nameBox.setText(cursor.getString(1));
-          binding.phoneBox.setText(cursor.getString(2));
-
-          binding.insertBtn.setText("Update Now");
-          binding.insertBtn.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View view) {
-                boolean isUpdated = helper.updateOrder(
-                         binding.nameBox.getText().toString(),
-                         binding.phoneBox.getText().toString(),
-                         Integer.parseInt(binding.priceLbl.getText().toString()),
-                         image,
-                         binding.detailDescription.getText().toString(),
-                         binding.nameBox.getText().toString(),
-                         1,
-                         id
-                 );
-
-                 if(isUpdated)
-                     Toast.makeText(DetailActivity.this, "Updated", Toast.LENGTH_SHORT).show();
-                 else
-                     Toast.makeText(DetailActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-
-              }
-          });
-      }
+        return isValid;
     }
 
     private void addFoodQuantity() {
@@ -118,7 +158,7 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void subtractFoodQuantity() {
-        if(foodQty > 1) {
+        if (foodQty > 1) {
             foodQty -= 1;
             binding.detailFoodItemQtyTv.setText(String.valueOf(foodQty));
         } else {
